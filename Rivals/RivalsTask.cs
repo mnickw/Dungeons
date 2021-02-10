16a6309bac3158ef2b6420f9e1b0ff6608ddd368
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-02-10 09:46:49 +0300

    Implement Rivals/RivalsTask.cs file

--- Rivals/RivalsTask.cs
+++ Rivals/RivalsTask.cs
@@ -12,7 +12,32 @@ namespace Rivals
 	{
 		public static IEnumerable<OwnedLocation> AssignOwners(Map map)
 		{
-			yield break;
+			var visited = new HashSet<Point>();
+			var queue = new Queue<OwnedLocation>();
+
+			for (int i = 0; i < map.Players.Length; i++)
+			{
+				visited.Add(map.Players[i]);
+				queue.Enqueue(new OwnedLocation(i, map.Players[i], 0));
+			}
+
+			while (queue.Count != 0)
+			{
+				var node = queue.Dequeue();
+				yield return node;
+				var incidentNodes = new Point[]
+				{
+					new Point(node.Location.X, node.Location.Y+1),
+					new Point(node.Location.X, node.Location.Y-1),
+					new Point(node.Location.X+1, node.Location.Y),
+					new Point(node.Location.X-1, node.Location.Y)
+				}.Where(point => map.InBounds(point) && map.Maze[point.X, point.Y] != MapCell.Wall);
+				foreach (var nextNode in incidentNodes.Where(incidentNode => !visited.Contains(incidentNode)))
+				{
+					visited.Add(nextNode);
+					queue.Enqueue(new OwnedLocation(node.Owner, nextNode, node.Distance + 1));
+				}
+			}
 		}
 	}
 }
